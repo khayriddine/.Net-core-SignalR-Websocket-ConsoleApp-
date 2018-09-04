9e3d32a80c5e534939ca1e65b5e1f3d8744b96f6
DexterMorgan <barpaw@gmail.com>
2018-09-04 17:38:12 +0200

    Init

--- ConsoleApp5/Program.cs
+++ ConsoleApp5/Program.cs
@@ -1,12 +1,99 @@
-﻿using System;
+﻿using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.IO;
+using Microsoft.AspNetCore;
+using Microsoft.AspNetCore.SignalR;
+using System.Threading.Tasks;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.Builder;
 
 namespace ConsoleApp5
 {
     class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+
+          //  var ConsOut = Console.Out;  //Save the reference to the old out value (The terminal)
+          //  Console.SetOut(new StreamWriter(Stream.Null)); //Remove console output
+
+
+
+            var host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .UseUrls("http://localhost:5050")
+                .Build();                     //Modify the building per your needs
+
+            host.Start();                     //Start server non-blocking
+
+       //     Console.SetOut(ConsOut);          //Restore output
+
+            //Regular console code
+            while (true)
+            {
+                Console.WriteLine(Console.ReadLine());
+            }
+        }
+    }
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddSignalR();
+
+        }
+
+        public void ConfigureProduction(IApplicationBuilder app)
+        {
+            app.UseSignalR(route =>
+{
+    route.MapHub<MyHub>("/chathub");
+});
+
+        }
+    }
+
+    public static class UserHandler
+    {
+        public static HashSet<string> ConnectedIds = new HashSet<string>();
+    }
+
+
+    public class MyHub : Hub
+    {
+        public async Task SendMessage(string user, string message)
+        {
+            await Clients.All.SendAsync("ReceiveMessage", user, message);
+            Console.WriteLine($"User: {user} says {message}");
+        }
+
+        public Task SendMessageToCaller(string message)
+        {
+            return Clients.Caller.SendAsync("ReceiveMessage", message);
+        }
+
+        public Task SendMessageToGroups(string message)
+        {
+            List<string> groups = new List<string>() { "SignalR Users" };
+            return Clients.Groups(groups).SendAsync("ReceiveMessage", message);
+        }
+
+        public override async Task OnConnectedAsync()
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, "SignalR Users");
+            UserHandler.ConnectedIds.Add(Context.ConnectionId);
+            Console.WriteLine($"Connected user: {Context.ConnectionId}");
+            await base.OnConnectedAsync();
+        }
+
+        public override async Task OnDisconnectedAsync(Exception exception)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, "SignalR Users");
+            UserHandler.ConnectedIds.Remove(Context.ConnectionId);
+            Console.WriteLine($"DIsconnected user: {Context.ConnectionId}");
+            await base.OnDisconnectedAsync(exception);
         }
     }
 }
---
+++ SignalR Client/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using Microsoft.AspNetCore.SignalR.Client;
+using Microsoft.Extensions.Logging;
+
+namespace SignalR_Client
+{
+    class Program
+    {
+        static HubConnection connection;
+        static ConcurrentBag<string> messagesList = new ConcurrentBag<string>();
+        static void Main(string[] args)
+        {
+            connection = new HubConnectionBuilder()
+            .WithUrl("http://localhost:5050/chathub")
+            .Build();
+
+
+            ConnectToServer();
+
+            var str = "{\r\n    \"glossary\": {\r\n        \"title\": \"example glossary\",\r\n\t\t\"GlossDiv\": {\r\n            \"title\": \"S\",\r\n\t\t\t\"GlossList\": {\r\n                \"GlossEntry\": {\r\n                    \"ID\": \"SGML\",\r\n\t\t\t\t\t\"SortAs\": \"SGML\",\r\n\t\t\t\t\t\"GlossTerm\": \"Standard Generalized Markup Language\",\r\n\t\t\t\t\t\"Acronym\": \"SGML\",\r\n\t\t\t\t\t\"Abbrev\": \"ISO 8879:1986\",\r\n\t\t\t\t\t\"GlossDef\": {\r\n                        \"para\": \"A meta-markup language, used to create markup languages such as DocBook.\",\r\n\t\t\t\t\t\t\"GlossSeeAlso\": [\"GML\", \"XML\"]\r\n                    },\r\n\t\t\t\t\t\"GlossSee\": \"markup\"\r\n                }\r\n            }\r\n        }\r\n    }\r\n}";
+
+
+            SendMessage("Bob", str);
+
+
+
+            while (true)
+            {
+
+                Console.WriteLine(Console.ReadLine());
+            }
+        }
+
+        private static async void ConnectToServer()
+        {
+            connection.On<string, string>("ReceiveMessage", (user, message) =>
+            {
+                var newMessage = $"{user}: {message}";
+                messagesList.Add(newMessage);
+                Console.WriteLine(newMessage);
+            });
+
+            try
+            {
+                await connection.StartAsync();
+                messagesList.Add("Connection started");
+                Console.WriteLine("Connection started");
+            }
+            catch (Exception ex)
+            {
+                messagesList.Add(ex.Message);
+            }
+        }
+
+        private static async void SendMessage(string user, string message)
+        {
+            try
+            {
+                await connection.InvokeAsync("SendMessage", user, message);
+            }
+            catch (Exception ex)
+            {
+                messagesList.Add(ex.Message);
+            }
+        }
+    }
+}
